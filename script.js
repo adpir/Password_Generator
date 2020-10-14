0358505cb9d56f7fd6c6b0b3e14c631e3d146b58
Alice Piar <adpir30@gmail.com>
2020-10-14 13:01:41 -0500

    Added Length validation and prompt validation

---
+++ script.js
@@ -0,0 +1,51 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+function generatePassword() {
+  let passwordlenght =parseInt(prompt("Enter Passowrd length(8-120)"))
+  if( passwordlenght > 7 && passwordlenght < 121){
+    getuserchoice(passwordlenght)
+  }
+  else{
+    generatePassword()
+  }
+}
+
+function getuserchoice(passwordlenght){
+  console.log("Valid length",passwordlenght)
+  let yesupper = false;
+  let yeslower =false;
+  let yesspecials = false;
+  let yesnumber = false;
+
+  while(!yeslower && !yesupper && !yesspecials && !yesnumber){
+      yesnumber = confirm("Do you want numbers?");
+      yesupper= confirm("Do you want upper letters?");
+      yesspecials= confirm( "Do you want to included specials characters?");
+      yeslower=confirm("do you want lower letters?");
+  }
+  let validcharacters = [];
+  if (yesnumber){
+    validcharacters =validcharacters + "1234567890"
+  }
+  if (yesupper){
+    validcharacters =validcharacters + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+  if (yesspecials){
+    validcharacters =validcharacters + "!@#$%^&*()_+="
+  }
+  if (yeslower){
+    validcharacters =validcharacters + "abcdefghijklmnopqrstuvwxyz"
+  }
+  console.log (validcharacters)
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
